a1774985c9c3b7369b1a2f94b72eedf7dde98cad
felipereblin <32180600+felipereblin@users.noreply.github.com>
2020-04-11 15:33:58 -0300

    ajuste de arquivos

--- src/org/AG.java
+++ src/org/AG.java
@@ -1,12 +1,7 @@
 package org;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
 
 public class AG {
 	public static void main(String[] args) {
